d3fc26609c5ea30fcf7f60c58a435a25fee8dddf
olol85 <olol85@gmail.com>
2023-12-17 02:04:37 +0100

    table subtests

--- job_manager_test.go
+++ job_manager_test.go
@@ -7,40 +7,47 @@ import (
 
 func TestBadSchemes(t *testing.T) {
 	testCases := []struct {
-		name   string
-		input  string
-		output string
+		name                string
+		input               string
+		output              string
+		expect_scheme_error bool
 	}{
 		{
-			name:   "bad input scheme",
-			input:  "badscheme://path/to/data/",
-			output: "scheme://path/to/data/",
+			name:                "bad input scheme",
+			input:               "badscheme://path/to/data/",
+			output:              "scheme://path/to/data/",
+			expect_scheme_error: true,
 		},
 		{
-			name:   "bad output scheme",
-			input:  "file:///path/to/data/",
-			output: "badscheme://path/to/data/",
+			name:                "bad output scheme",
+			input:               "file:///path/to/data/",
+			output:              "badscheme://path/to/data/",
+			expect_scheme_error: true,
+		},
+		{
+			name:                "good schemes",
+			input:               "file:///path/to/data/",
+			output:              "scheme://path/to/data/",
+			expect_scheme_error: false,
 		},
 	}
 	for _, tc := range testCases {
-		jm := NewJobManager(*NewMockUploader())
-		_, err := jm.create(tc.input, tc.output)
+		t.Run(tc.name, func(t *testing.T) {
 
-		var got *schemeError
-		isSchemeError := errors.As(err, &got)
+			jm := NewJobManager(*NewMockUploader())
+			_, err := jm.create(tc.input, tc.output)
 
-		if !isSchemeError {
-			t.Fatalf("[%s] expected a schemeError, got %T", tc.name, err)
-		}
+			var got *schemeError
+			isSchemeError := errors.As(err, &got)
+
+			if tc.expect_scheme_error && (!isSchemeError) {
+				t.Fatalf("[%s] expected scheme error %v, got %T", tc.name, tc.expect_scheme_error, err)
+			} else if (!tc.expect_scheme_error) && isSchemeError {
+				t.Fatalf("[%s] did not expect scheme error %v, got %T", tc.name, tc.expect_scheme_error, err)
+
+			}
+
+		})
 
 	}
-}
-
-func TestCreate(t *testing.T) {
-	jm := NewJobManager(*NewMockUploader())
-	_, err := jm.create("file:///path/to/dir/", "scheme://path/to/data/")
-	if err != nil {
-		t.Fatalf("expected null error creating job: %v", err)
-	}
-
 }
